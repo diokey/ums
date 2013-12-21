df9117f2d77d6f2d6c6d6ceec64d85a84506d25b
Diokey Olivier <odusabimana@bluesquare.org>
2013-12-21 20:16:51 +0200

    Assigning courses to classes

--- JavaSource/org/gs/controller/ClassCoursesBean.java
+++ JavaSource/org/gs/controller/ClassCoursesBean.java
@@ -4,11 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
-import javax.faces.event.AjaxBehaviorEvent;
-import javax.faces.event.ValueChangeEvent;
 
 import org.gs.dao.ClassCourseDAO;
 import org.gs.dao.CourseDAO;
@@ -17,6 +16,7 @@ import org.gs.model.ClassCourse;
 import org.gs.model.Course;
 import org.gs.model.SchoolPeriod;
 import org.gs.model.Structure;
+import org.gs.util.FacesUtil;
 
 @ManagedBean
 @ViewScoped
@@ -25,13 +25,16 @@ public class ClassCoursesBean {
 	
 	
 	public void addCourses() {
-		System.out.println("Adding courses...");
+		
 		for(Course c : this.selectedCourses) {
 			ClassCourse classCourse = new ClassCourse();
 			classCourse.setCourse(c);
 			classCourse.setCourseId(c.getCourseId());
+			classCourse.setCourseDescription(c.getCourseDescription());
 			
 			this.classCourses.add(classCourse);
+			
+			this.modifiedClassCourses.add(classCourse);			
 		}
 		
 		this.allCourses.removeAll(this.selectedCourses);
@@ -41,8 +44,7 @@ public class ClassCoursesBean {
 			this.selectedCourses.clear();
 	}
 	
-	public void saveSelectedCourses() {
-		
+	public void saveSelectedCourses() {		
 		
 		ClassCourseDAO ccDao = new ClassCourseDAO();
 		
@@ -52,39 +54,161 @@ public class ClassCoursesBean {
 		
 		SchoolPeriod sp = this.schoolTreeBean.getSelectedSchoolPeriod();
 		
-		for(ClassCourse cc : this.classCourses) {
+		/*
+		 * for(ClassCourse cc : this.classCourses) {
+		 
 			cc.setClassId(classe.getId());
 			cc.setPeriodId(sp.getSchoolPeriodId());
 			cc.setCourseId(cc.getCourse().getCourseId());
 			
 			ccDao.create(cc);
 		}
+		 */
+		
+		String message = "";
+		int addedCounter = 0;
+		int modifiedCounter = 0;
+		int deletedCounter = 0;
+		
+		for(ClassCourse cc : this.deletedClassCourses) {
+			
+			System.out.println(cc.getCourse().getCourseName());
+			cc.setClassId(classe.getId());
+			cc.setPeriodId(sp.getSchoolPeriodId());
+			cc.setCourseId(cc.getCourse().getCourseId());
+			
+			if(cc.getClassCourseId()!=0) {
+				ccDao.softDelete(cc);
+				deletedCounter++;
+			} 
+		}
+		
+		
+		for(ClassCourse cc : this.modifiedClassCourses) {
+			System.out.println(cc.getCourse().getCourseName());
+			cc.setClassId(classe.getId());
+			cc.setPeriodId(sp.getSchoolPeriodId());
+			cc.setCourseId(cc.getCourse().getCourseId());
+			
+			if(cc.getClassCourseId()==0) {
+				if(!ccDao.isSoftDeleted(cc))
+					ccDao.create(cc);
+				else
+					ccDao.restore(cc);
+				
+				addedCounter++;
+				
+			} else {
+				ccDao.update(cc);
+				modifiedCounter++;
+			}
+		}
+		
+		boolean comma = false;
+		
+		if(deletedCounter!=0) {
+			message = deletedCounter+ " courses deleted";
+			comma = true;
+		}
+		
+		if(addedCounter!=0) {
+			message+=comma?"":" , ";
+			message+= addedCounter+" courses added";
+			comma = true;
+		}
+		
+		if(modifiedCounter!=0) {
+			message+=comma?"":" , ";
+			message+= modifiedCounter+" courses modified";
+		}
+		
+		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "", message);
+		
+		FacesUtil.addMessage(null, msg);
+		
+		if(this.selectedCourses!=null)		
+			this.selectedClassCourses.clear();
+		if(this.modifiedClassCourses!=null)
+			this.modifiedClassCourses.clear();
+		if(this.deletedClassCourses!=null)
+			this.deletedClassCourses.clear();
+		
+		if(this.filteredCourses!=null)
+			this.filteredCourses.clear();
+		if(this.selectedCourses!=null)
+			this.selectedCourses.clear();
 		
 	}
 	
 	
-	public void test(String index) {
+	public void onClassCourseRemove() {
+		for (ClassCourse c : this.selectedClassCourses) {
+			this.allCourses.add(c.getCourse());
+			this.deletedClassCourses.add(c);
+			if(this.modifiedClassCourses.contains(c)) {
+				this.modifiedClassCourses.remove(c);
+			}
+		}
 		
+		this.classCourses.removeAll(this.selectedClassCourses);
+		this.selectedClassCourses.clear();
+		if(this.filteredCourses!=null)
+			this.filteredCourses.clear();
+	}
+	
+	public void classCourseModified(Integer index) {
+		ClassCourse modified = this.classCourses.get(index);
 		
+		if(modified!=null) {
+			if(!this.modifiedClassCourses.contains(modified)) {
+				this.modifiedClassCourses.add(modified);
+			}
+		}
+	}
+	
+	private void removeAssignedCourses() {
+		for(ClassCourse c : this.classCourses) {
+			removeIfExists(c.getCourse());
+		}
+	}
+	
+	private void removeIfExists(Course course) {
+		Course found = null;
+		for(Course c : this.allCourses) {
+			if(c.getCourseId()==course.getCourseId()) {
+				found = c;
+				System.out.println("To be removed : "+found.getCourseName());
+				break;
+			}
+				
+		}
+		
+		if(found!=null)
+			this.allCourses.remove(found);	
+	}
+	
+	public void onClassChanged() {
+		
+		this.init();
 	}
 	
 	public ClassCoursesBean() {
 		// TODO Auto-generated constructor stub
 		
 		this.classCourses = new ArrayList<ClassCourse>();
+		this.modifiedClassCourses = new ArrayList<ClassCourse>();
+		this.deletedClassCourses = new ArrayList<ClassCourse>();
+		
 		this.courseDao = new CourseDAO();
 		this.classCourseDao = new ClassCourseDAO();
 		
-		
-		allCourses = this.courseDao.findAll();
 	}
 	
-	private void removeAssignedCourses() {
-		
-	}
 	
 	@PostConstruct
 	private void init() {
+		
+		allCourses = this.courseDao.findAll();
 		
 		this.schoolTreeBean.getSelectedNode();
 		
@@ -95,6 +219,7 @@ public class ClassCoursesBean {
 		this.classCourses = this.classCourseDao.findClassCourses(classe.getId(), sp.getSchoolPeriodId());
 		
 		System.out.println("ALl courses "+this.classCourses.size());
+		this.removeAssignedCourses();
 	}
 		
 	public List<Course> getAllCourses() {
@@ -136,14 +261,6 @@ public class ClassCoursesBean {
 		this.selectedClassCourses = selectedClassCourses;
 	}
 	
-	public ClassCourse getSelectedClassCourse() {
-		return selectedClassCourse;
-	}
-
-	public void setSelectedClassCourse(ClassCourse selectedClassCourse) {
-		this.selectedClassCourse = selectedClassCourse;
-	}
-	
 	public SchoolTreeBean getSchoolTreeBean() {
 		return schoolTreeBean;
 	}
@@ -153,14 +270,13 @@ public class ClassCoursesBean {
 	}
 
 
-
-
 	private List<Course> allCourses;
 	private List<Course> selectedCourses;
 	private List<Course> filteredCourses;
 	private List<ClassCourse> classCourses;
 	private List<ClassCourse> selectedClassCourses;
-	private ClassCourse selectedClassCourse;
+	private List<ClassCourse> modifiedClassCourses;
+	private List<ClassCourse> deletedClassCourses;
 	private CourseDAO courseDao;
 	private ClassCourseDAO classCourseDao;
 	
--- JavaSource/org/gs/dao/ClassCourseDAO.java
+++ JavaSource/org/gs/dao/ClassCourseDAO.java
@@ -61,13 +61,176 @@ public class ClassCourseDAO extends DAO<ClassCourse>{
 	@Override
 	public boolean update(ClassCourse object) {
 		// TODO Auto-generated method stub
-		return false;
+		String sql = "UPDATE class_course SET class_id=?, course_id=?, period_id=?, hours=?, credits=?, course_description=?, modified_on = NOW() "
+				+ "WHERE class_course_id = ?) ";
+				
+		
+		PreparedStatement pst = null;
+		boolean rep = false;
+		
+		try {
+			pst = this.con.prepareStatement(sql);
+			pst.setInt(1, object.getClassId());
+			pst.setInt(2, object.getCourseId());
+			pst.setInt(3, object.getPeriodId());
+			pst.setInt(4, object.getHours());
+			pst.setInt(5, object.getCredits());
+			pst.setString(6, object.getCourseDescription());
+			pst.setInt(7, object.getClassCourseId());
+			
+			rep = pst.executeUpdate()>0;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				pst.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return rep;
 	}
 
 	@Override
 	public boolean delete(ClassCourse object) {
 		// TODO Auto-generated method stub
-		return false;
+		String sql = "DELETE FROM class_course where class_course_id = ?";
+		
+		PreparedStatement pst = null;
+		boolean rep = false;
+		
+		try {
+			pst = this.con.prepareStatement(sql);
+			pst.setInt(1, object.getClassCourseId());
+						
+			rep = pst.executeUpdate()>0;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				pst.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return rep;
+	}
+	
+	public boolean softDelete(ClassCourse object) {
+		String sql = "UPDATE class_course SET deleted = ?"
+				+ " WHERE class_course_id = ? ";				
+		
+		PreparedStatement pst = null;
+		boolean rep = false;
+		
+		try {
+			pst = this.con.prepareStatement(sql);
+			pst.setBoolean(1, true);
+			pst.setInt(2, object.getClassCourseId());
+			
+			rep = pst.executeUpdate()>0;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				pst.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return rep;
+	}
+	
+	public boolean restore(ClassCourse object) {
+		String sql = "UPDATE class_course SET hours=?, credits=?, course_description=?, created_on = NOW(), modified_on = NOW(), deleted = ?"
+				+ " WHERE class_id = ? and period_id = ? and course_id = ?) ";				
+		
+		PreparedStatement pst = null;
+		boolean rep = false;
+		
+		try {
+			pst = this.con.prepareStatement(sql);
+	
+			
+			pst.setInt(1, object.getHours());
+			pst.setInt(2, object.getCredits());
+			pst.setString(3, object.getCourseDescription());
+			pst.setBoolean(4, false);
+			pst.setInt(5, object.getClassId());
+			pst.setInt(6, object.getPeriodId());
+			pst.setInt(7, object.getCourseId());
+						
+			rep = pst.executeUpdate()>0;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				pst.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return rep;
+	}
+	
+	public boolean isSoftDeleted(ClassCourse object) {
+		String sql = "SELECT class_course_id FROM class_course where class_id = ? and period_id = ? and course_id = ? and deleted = ?";
+		ResultSet res = null;
+		PreparedStatement pst = null;
+		boolean result = false;
+		
+		try {
+			pst = this.con.prepareStatement(sql);
+			pst.setInt(1, object.getClassId());
+			pst.setInt(2, object.getPeriodId());
+			pst.setInt(3, object.getCourseId());
+			pst.setBoolean(4, true);
+			
+			res = pst.executeQuery();
+			
+			result = res.next() ;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				res.close();
+				pst.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		return result;
 	}
 
 	@Override
@@ -120,9 +283,8 @@ public class ClassCourseDAO extends DAO<ClassCourse>{
 	public List<ClassCourse> findClassCourses(int classId, int periodId) {
 		List<ClassCourse> courses = new ArrayList<ClassCourse>();
 		
-		String sql = "SELECT class_course_id FROM class_course where class_id = ? and period_id = ?";
-		String test = "SELECT class_course_id FROM class_course where class_id = "+classId+" and period_id ="+periodId;
-		System.out.println(test);
+		String sql = "SELECT class_course_id FROM class_course where class_id = ? and period_id = ? and deleted=?";
+		
 		PreparedStatement pst = null;
 		ResultSet res = null;
 		
@@ -130,6 +292,7 @@ public class ClassCourseDAO extends DAO<ClassCourse>{
 			pst = this.con.prepareStatement(sql);
 			pst.setInt(1, classId);
 			pst.setInt(2, periodId);
+			pst.setBoolean(3, false);
 			
 			res = pst.executeQuery();
 			
